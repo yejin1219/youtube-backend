de73a4653a208b34f55d7b54b5c92cbb4b0f1b05
yejin1219 <133620338+yejin1219@users.noreply.github.com>
2023-09-07 10:49:53 +0900

    feat:jpa service,controller 작성

---
+++ src/main/java/com/kh/youtube/controller/CategoryController.java
@@ -0,0 +1,64 @@
+package com.kh.youtube.controller;
+
+import com.kh.youtube.domain.Category;
+import com.kh.youtube.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/*")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService service;
+
+    @GetMapping("/category")
+    public ResponseEntity<List<Category>> showAll(){
+        try {
+              return ResponseEntity.status(HttpStatus.OK).body(service.showAll());
+        }catch (Exception e){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // build() = body(null)과 같음
+        }
+    }
+
+    @GetMapping("/category/{id}")
+    public ResponseEntity<Category>show(@PathVariable int id){
+          try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.show(id));
+          }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+          }
+      }
+
+    @PostMapping("/category")
+    public ResponseEntity<Category>create(@PathVariable Category category){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.create(category));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PutMapping("/category")
+    public ResponseEntity<Category>update(@PathVariable Category category){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.update(category));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<Category>delete(@PathVariable int id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(service.delete(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+}
+
--- src/main/java/com/kh/youtube/service/CategoryService.java
+++ src/main/java/com/kh/youtube/service/CategoryService.java
@@ -1,13 +1,39 @@
 package com.kh.youtube.service;
 
 
+import com.kh.youtube.domain.Category;
 import com.kh.youtube.repo.CategoryDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryDAO dao;
+
+    public List<Category> showAll(){
+        return dao.findAll();
+    }
+
+    public Category show(int code){
+
+        return dao.findById(code).orElse(null);
+    }
+
+    public Category create(Category category){
+        return dao.save(category); //추가, 수정 할 때 메소드는 save
+    }
+    public Category update(Category category){
+        return dao.save(category);
+    }
+
+    public Category delete(int code){
+        Category data = dao.findById(code).orElse(null);
+        dao.delete(data);
+        return data;
+    }
 }
